bf83329732ae6de563ac22b0119e9ff95997b799
Zak <zakmcrae@gmail.com>
2021-04-01 15:24:23 -0400

    Database models for User, Post, Reply

---
+++ models.py
@@ -0,0 +1,67 @@
+import datetime
+from typing import Optional, List
+
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLAlchemyBase = declarative_base()
+
+
+class Reply(SQLAlchemyBase):
+    __tablename__ = "replies"
+
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    title: str = sa.Column(sa.String, nullable=False)
+    body: str = sa.Column(sa.TEXT, nullable=False)
+    date_created: datetime = sa.Column(
+        sa.DATETIME,
+        nullable=False,
+        default=datetime.datetime.utcnow,
+    )
+    date_modified: datetime = sa.Column(sa.DATETIME)
+    user_id = sa.Column(sa.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    post_id = sa.Column(sa.ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
+
+
+class Post(SQLAlchemyBase):
+    __tablename__ = "posts"
+
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    title: str = sa.Column(sa.String, nullable=False)
+    body: str = sa.Column(sa.TEXT, nullable=False)
+    date_created: datetime = sa.Column(
+        sa.DATETIME,
+        nullable=False,
+        default=datetime.datetime.utcnow,
+    )
+    date_modified: datetime = sa.Column(sa.DATETIME)
+    user_id = sa.Column(sa.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    replies: Optional[List[Reply]] = orm.relationship(
+        "Reply",
+        order_by="asc(Reply.date_created)",
+        cascade="all,delete-orphan",
+    )
+
+
+class User(SQLAlchemyBase):
+    __tablename__ = "users"
+
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    username: str = sa.Column(sa.String(24), unique=True, nullable=False)
+    email: str = sa.Column(sa.String(120), unique=True, nullable=False)
+    hs_password: str = sa.Column(sa.String(60), nullable=False)
+    posts: Optional[List[Post]] = orm.relationship(
+        "Post",
+        order_by="asc(Post.date_created)",
+        cascade="all,delete-orphan",
+    )
+    replies: Optional[List[Reply]] = orm.relationship(
+        "Reply",
+        order_by="asc(Reply.date_created)",
+        cascade="all,delete-orphan",
+    )
+
+    def __repr__(self):
+        return f"User:{self.username}, ID:{self.id}"
