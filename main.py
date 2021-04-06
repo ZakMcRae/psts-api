2f993b8a0a87d1662ad1e9e6c8ef0619aea4340a
Zak <zakmcrae@gmail.com>
2021-04-06 13:15:10 -0400

    added config.json for secrets/env variables

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import fastapi
 import uvicorn
 
@@ -10,6 +12,7 @@ api = fastapi.FastAPI()
 def configure():
     configure_db()
     configure_routing()
+    configure_env_variables()
 
 
 def configure_db():
@@ -18,6 +21,13 @@ def configure_db():
 
 def configure_routing():
     api.include_router(routes.router)
+
+
+def configure_env_variables():
+    with open("config.json") as fin:
+        config_info = json.load(fin)
+
+    routes.secret_key = config_info.get("secret_key")
 
 
 if __name__ == "__main__":
