6b7eb0bd6192af09ffda32c333799b4406a7a977
Zak <zakmcrae@gmail.com>
2021-04-07 15:07:57 -0400

    - FastAPI oauth2_scheme moved to dependencies.py - secret_key/env variables now in config.py (gitignored) - routes to generate tokens/pull user info

--- BlogAPI/routers/routes.py
+++ BlogAPI/routers/routes.py
@@ -1,41 +1,53 @@
-from fastapi import Depends, APIRouter
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+import datetime
 
+import jwt
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from BlogAPI.config import config_settings
 from BlogAPI.db import db_session
 from BlogAPI.db.SQLAlchemy_models import User
-
 from BlogAPI.util.mock_data import (
     add_sample_users,
     add_sample_posts,
     add_sample_replies,
     add_sample_follows,
 )
+from BlogAPI.util.utils import get_current_user, authenticate_user
 
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-secret_key = None
+
+
+@router.get("/me")
+def get_user(user=Depends(get_current_user)):
+    return {"username": user.username, "email": user.email, "id": user.id}
 
 
 @router.post("/token")
-def token(form_data: OAuth2PasswordRequestForm = Depends()):
-    return {"access_token": form_data.username + "token"}
+def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        return HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Username or Password",
+        )
 
+    user_info = {
+        "id": user.id,
+        "username": user.username,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=30),
+    }
 
-@router.get("/test_token")
-def test_token(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+    token = jwt.encode(user_info, config_settings.secret_key)
+    return {"access_token": token, "token_type": "bearer"}
 
 
 @router.get("/test")
 def test():
-    return "test"
-
-
-@router.get("/")
-def home():
-    # display all routes for quick access for testing - temp home
-    all_routes = [f"http://127.0.0.1:8000/{route.name}" for route in router.routes]
-    return all_routes
+    # username = "elliot"
+    # session = db_session.create_session()
+    # user = session.query(User).filter_by(username=username).first()
+    return authenticate_user("theo", "123")
 
 
 @router.get("/query_users")
