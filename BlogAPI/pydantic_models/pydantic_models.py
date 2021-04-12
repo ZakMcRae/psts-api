5c3cd0763e999349e2a67768035f50bd95b72ee5
Zak <zakmcrae@gmail.com>
2021-04-12 13:03:17 -0400

    added models for creating and updating blog posts with example for OpenAPI docs

--- BlogAPI/pydantic_models/pydantic_models.py
+++ BlogAPI/pydantic_models/pydantic_models.py
@@ -35,6 +35,20 @@ class UserOut(BaseModel):
         }
 
 
+class NewPostIn(BaseModel):
+    title: str
+    body: str
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "title": "My First Post",
+                "body": "Welcome to my blog",
+            }
+        }
+
+
 class PostOut(BaseModel):
     id: int
     title: str
@@ -52,6 +66,42 @@ class PostOut(BaseModel):
                 "body": "Welcome to my blog",
                 "date_created": "2021-04-07 19:41:00.769100",
                 "date_modified": "null",
+                "user_id": 4376,
+            }
+        }
+
+
+class UpdatePostIn(BaseModel):
+    title: Optional[str]
+    body: Optional[str]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "title": "My First Updated Post",
+                "body": "Welcome to my blog.",
+            }
+        }
+
+
+class UpdatePostOut(BaseModel):
+    id: int
+    title: str
+    body: str
+    date_created: datetime
+    date_modified: Optional[datetime]
+    user_id: int
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "id": 74167,
+                "title": "My First Updated Post",
+                "body": "Welcome to my blog",
+                "date_created": "2021-04-07 19:41:00.769100",
+                "date_modified": "2021-04-08 19:43:00.769100",
                 "user_id": 4376,
             }
         }
@@ -76,3 +126,10 @@ class ReplyOut(BaseModel):
                 "user_id": 4376,
             }
         }
+
+
+class FollowOut(BaseModel):
+    success: str
+
+    class Config:
+        schema_extra = {"example": {"success": "Matt now following Carin"}}
