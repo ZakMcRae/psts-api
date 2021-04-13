fdba90d0ae600cd823485b7bf1d0712f45480496
Zak <zakmcrae@gmail.com>
2021-04-13 16:04:32 -0400

    renamed router - to be removed long term

--- BlogAPI/main.py
+++ BlogAPI/main.py
@@ -2,7 +2,7 @@ import fastapi
 import uvicorn
 
 from BlogAPI.db import db_session
-from BlogAPI.routers import routes, user_routes, post_routes
+from BlogAPI.routers import temp_routes, user_routes, post_routes
 
 api = fastapi.FastAPI()
 
@@ -17,7 +17,7 @@ def configure_db():
 
 
 def configure_routing():
-    api.include_router(routes.router)
+    api.include_router(temp_routes.router)  # delete once pytest tests ready
     api.include_router(user_routes.router)
     api.include_router(post_routes.router)
 
