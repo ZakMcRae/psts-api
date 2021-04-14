a0fdf8805849b84d799f0308a5b3e3ee3ae9e33b
Zak <zakmcrae@gmail.com>
2021-04-14 12:28:09 -0400

    nothing worthwhile - messing/learning testing

--- BlogAPI/tests/test_user.py
+++ BlogAPI/tests/test_user.py
@@ -1,5 +1,3 @@
-import json
-
 from fastapi.testclient import TestClient
 
 from BlogAPI.main import api
@@ -7,22 +5,27 @@ from BlogAPI.main import api
 client = TestClient(api)
 
 
-# def test_get_user():
-#     resp = client.get("/user/<user_id>?user_id=1")
-#     x = json.loads(resp.content)
-#     assert x == {"username": "zak", "email": "z@z.com", "id": 1}
-#     # assert resp.status_code == 200
+def test_get_test():
+    resp = client.get("/test")
 
+    print(resp.status_code)
+    print(resp.json())
 
-def test_letters():
-    resp = client.get("/h/<letters>?letters=abcd")
     assert resp.status_code == 200
-    assert resp.json() == {"a": 0, "b": 1, "c": 2, "d": 3}
+    assert resp.json() == "test1"
 
 
-# def test_token():
-#     body = {"username": "zak", "password": "123"}
-#     resp = client.post("http://127.0.0.1:8000/token", body)
-#     assert resp.status_code == 200
-#     assert resp.json() ==
-# todo how to test for token - ie random output
+def test_get_user():
+    resp = client.get("/user/<user_id>?user_id=1")
+    # resp = client.get("http://127.0.0.1:8000/user/<user_id>?user_id=1")
+    # resp = requests.get("http://127.0.0.1:8000/user/<user_id>?user_id=1")
+
+    print(resp.status_code)
+    print(resp.json())
+
+    assert resp.status_code == 200
+    assert resp.json() == {
+        "username": "testuser",
+        "email": "testuser@example.com",
+        "id": 1,
+    }
