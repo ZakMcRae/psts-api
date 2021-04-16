0539e84dde39f748fed553476d35b5585fcb74cb
Zak <zakmcrae@gmail.com>
2021-04-16 13:47:48 -0400

    added reply_routes to the main api added tags to routers for OpenAPI docs group by tag

--- BlogAPI/main.py
+++ BlogAPI/main.py
@@ -2,7 +2,7 @@ import fastapi
 import uvicorn
 
 from BlogAPI.db import db_session
-from BlogAPI.routers import temp_routes, user_routes, post_routes
+from BlogAPI.routers import temp_routes, user_routes, post_routes, reply_routes
 
 api = fastapi.FastAPI()
 
@@ -17,9 +17,10 @@ def configure_db():
 
 
 def configure_routing():
-    api.include_router(temp_routes.router)  # delete once pytest tests ready
-    api.include_router(user_routes.router)
-    api.include_router(post_routes.router)
+    api.include_router(temp_routes.router, tags=["Temp"])
+    api.include_router(user_routes.router, tags=["User"])
+    api.include_router(post_routes.router, tags=["Post"])
+    api.include_router(reply_routes.router, tags=["Reply"])
 
 
 if __name__ == "__main__":
