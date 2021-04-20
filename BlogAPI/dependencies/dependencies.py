1679ecd237afe06857751324166e92659f0e28e1
Zak <zakmcrae@gmail.com>
2021-04-20 13:20:52 -0400

    added get_db
    rewrote db connection/session for better testing
    can use fast api dependency_override for get_db

--- BlogAPI/dependencies/dependencies.py
+++ BlogAPI/dependencies/dependencies.py
@@ -1,3 +1,13 @@
 from fastapi.security import OAuth2PasswordBearer
+from BlogAPI.db.db_session import SessionLocal
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
