fad94377cbd93f6a38193aa21644e56a0a69b1ba
Zak <zakmcrae@gmail.com>
2021-04-20 13:20:30 -0400

    rewrote db connection
    rewrote db connection/session for better testing
    can use fast api dependency_override for get_db

--- BlogAPI/db/db_session.py
+++ BlogAPI/db/db_session.py
@@ -1,39 +1,12 @@
-from typing import Callable, Optional
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-import sqlalchemy as sa
-import sqlalchemy.orm as orm
-from sqlalchemy.orm import Session
+SQLALCHEMY_DATABASE_URL = "sqlite:///blog.db"
 
-from BlogAPI.db.SQLAlchemy_models import SQLAlchemyBase
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-__factory: Optional[Callable[[], Session]] = None
-
-
-def global_init():
-    global __factory
-
-    if __factory:
-        return
-
-    conn_str = "sqlite:///site.db"
-
-    # Adding check_same_thread = False. This can be an issue about
-    # creating / owner thread when cleaning up sessions, etc. This is a sqlite restriction
-    engine = sa.create_engine(
-        conn_str, echo=False, connect_args={"check_same_thread": False}
-    )
-    __factory = orm.sessionmaker(bind=engine)
-
-    SQLAlchemyBase.metadata.create_all(engine)
-
-
-def create_session() -> Session:
-    global __factory
-
-    if not __factory:
-        raise Exception("You must call global_init() before using this method.")
-
-    session: Session = __factory()
-    session.expire_on_commit = False
-
-    return session
+Base = declarative_base()
