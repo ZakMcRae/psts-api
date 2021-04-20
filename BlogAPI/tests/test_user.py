dbdfc6ede03352d5606876203bae6d7e22e16475
Zak <zakmcrae@gmail.com>
2021-04-20 13:51:42 -0400

    db rework for better testability
    rewrote db connection to get_db, allows dependency override from fastAPI
    first successful test with override_get_db

--- BlogAPI/tests/test_user.py
+++ BlogAPI/tests/test_user.py
@@ -1,26 +1,43 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from BlogAPI.main import api
+from BlogAPI.dependencies.dependencies import get_db
+from BlogAPI.db.SQLAlchemy_models import Base
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+api.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(api)
 
 
-def test_get_test():
-    resp = client.get("/test")
-
-    print(resp.status_code)
-    print(resp.json())
-
-    assert resp.status_code == 200
-    assert resp.json() == "test1"
-
-
 def test_get_user():
-    resp = client.get("/user/<user_id>?user_id=1")
+    resp = client.get("/get_user_test?user_id=1")
 
     assert resp.status_code == 200
     assert resp.json() == {
-        "username": "testuser",
-        "email": "testuser@example.com",
         "id": 1,
+        "email": "zak@example.com",
+        "username": "zaktest",
+        "hs_password": "password",
     }
