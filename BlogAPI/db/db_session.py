5295ec06f1c3af511b3b68ca56c6c4f9e68bd400
Zak <zakmcrae@gmail.com>
2021-04-23 10:33:15 -0400

    using test database during development
    have mock data and functions to reset test.db

--- BlogAPI/db/db_session.py
+++ BlogAPI/db/db_session.py
@@ -2,9 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# todo - change conn string to production not testing
+# Production db
+# SQLALCHEMY_DATABASE_URL = (
+#     r"sqlite:///C:\Users\Zak\PycharmProjects\BlogAPI\BlogAPI\db\blog.db"
+# )
 
+# This points the api/test client to test.db instead of blog.db
 SQLALCHEMY_DATABASE_URL = (
-    r"sqlite:///C:\Users\Zak\PycharmProjects\BlogAPI\BlogAPI\db\blog.db"
+    r"sqlite:///C:\Users\Zak\PycharmProjects\BlogAPI\BlogAPI\tests\test.db"
 )
 
 engine = create_engine(
