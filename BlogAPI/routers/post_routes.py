58919e551216cd36173afee4362979d0dd9b46c8
Zak <zakmcrae@gmail.com>
2021-04-23 15:54:53 -0400

    finished rework of endpoints after db rework
    tests for core functionality of all post endpoints

--- BlogAPI/routers/post_routes.py
+++ BlogAPI/routers/post_routes.py
@@ -1,13 +1,13 @@
 import datetime
-
-from fastapi import Depends, APIRouter, HTTPException
+from typing import List
 
 from fastapi import Depends, APIRouter
+from fastapi import HTTPException, Query
+from sqlalchemy import desc, asc
 from sqlalchemy.orm import Session
 from starlette import status
 
-from BlogAPI.db import db_session
-from BlogAPI.db.SQLAlchemy_models import Post
+from BlogAPI.db.SQLAlchemy_models import Post, Reply
 from BlogAPI.dependencies.dependencies import get_current_user, get_db
 from BlogAPI.pydantic_models.post_models import (
     NewPostIn,
@@ -15,6 +15,7 @@ from BlogAPI.pydantic_models.post_models import (
     UpdatePostOut,
     UpdatePostIn,
 )
+from BlogAPI.pydantic_models.reply_models import NewReplyIn, ReplyOut
 
 router = APIRouter()
 
@@ -93,134 +94,144 @@ def update_post(
     return post
 
 
-# @router.delete(
-#     "/post/<post-id>",
-#     responses={200: {"content": {"application/json": {"example": "success"}}}},
-# )
-# def delete_post(post_id, user=Depends(get_current_user)):
-#     """
-#     # Delete specified post
-#     Deletes post and all replies to the post from the database.
-#
-#     ---
-#
-#     ### Authorization Header
-#     Must include:
-#     ```
-#     {
-#         "Authorization": "Bearer {token}"
-#     }
-#     ```
-#     """
-#
-#
-#     try:
-#         post = db.query(Post).get(post_id)
-#
-#         if post.user_id != user.id:
-#             raise HTTPException(
-#                 status_code=status.HTTP_401_UNAUTHORIZED,
-#                 detail="This post belongs to another user",
-#             )
-#
-#     except AttributeError:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND, detail="This post does not exist"
-#         )
-#
-#     db.delete(post)
-#     db.commit()
-#     return "success"
-#
-#
-# @router.get("/post/<post-id>", response_model=PostOut)
-# def get_post(post_id):
-#     """
-#     # Return specified post
-#     """
-#
-#     post = db.query(Post).get(post_id)
-#     return post
-#
-#
-# @router.post("/post/post-id/reply", response_model=ReplyOut)
-# def create_reply(post_id: int, new_reply: NewReplyIn, user=Depends(get_current_user)):
-#     """
-#     # Create new reply
-#     Creates reply to specified post and stores it in the database.
-#
-#     ---
-#
-#     ### Authorization Header
-#     Must include:
-#     ```
-#     {
-#         "Authorization": "Bearer {token}"
-#     }
-#     ```
-#     """
-#
-#     reply = Reply(
-#         body=new_reply.body,
-#         user_id=user.id,
-#         username=user.username,
-#         post_id=post_id,
-#     )
-#     db.add(reply)
-#     db.commit()
-#     return reply
-#
-#
-# @router.get("/post/<post-id>/replies", response_model=List[ReplyOut])
-# def get_replies(
-#     post_id: int,
-#     skip: int = 0,
-#     limit: int = Query(10, ge=0, le=25),
-#     sort_newest_first: bool = Query(True, alias="sort-newest-first"),
-# ):
-#     """
-#     # Returns all replies to specified post
-#     Use skip and limit for pagination.\\
-#     Sortable by date created (by default returns newest).
-#     """
-#
-#
-#     if sort_newest_first:
-#         replies = (
-#             db.query(Reply)
-#             .order_by(desc(Reply.date_created))
-#             .filter(Reply.post_id == post_id)
-#             .offset(skip)
-#             .limit(limit)
-#             .all()
-#         )
-#
-#     else:
-#         replies = (
-#             db.query(Reply)
-#             .order_by(asc(Reply.date_created))
-#             .filter(Reply.post_id == post_id)
-#             .offset(skip)
-#             .limit(limit)
-#             .all()
-#         )
-#
-#     return replies
-#
-#
-# @router.get("/posts/recent", response_model=List[PostOut])
-# def get_recent_posts(skip: int = 0, limit: int = Query(10, ge=0, le=25)):
-#     """
-#     # Returns list of recent posts from all users
-#     Useful for a home/front page blog site before login
-#     """
-#
-#     posts = (
-#         db.query(Post)
-#         .order_by(desc(Post.date_created))
-#         .offset(skip)
-#         .limit(limit)
-#         .all()
-#     )
-#
-#     return posts
+@router.delete(
+    "/post/<post-id>",
+    responses={
+        200: {"content": {"application/json": {"example": {"detail": "success"}}}}
+    },
+)
+def delete_post(
+    post_id,
+    user=Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    """
+    # Delete specified post
+    Deletes post and all replies to the post from the database.
+
+    ---
+
+    ### Authorization Header
+    Must include:
+    ```
+    {
+        "Authorization": "Bearer {token}"
+    }
+    ```
+    """
+
+    try:
+        post = db.query(Post).get(post_id)
+
+        if post.user_id != user.id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="This post belongs to another user",
+            )
+
+    except AttributeError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="This post does not exist"
+        )
+
+    db.delete(post)
+    db.commit()
+    return {"detail": "success"}
+
+
+@router.get("/post/<post-id>", response_model=PostOut)
+def get_post(post_id, db: Session = Depends(get_db)):
+    """
+    # Return specified post
+    """
+
+    post = db.query(Post).get(post_id)
+    return post
+
+
+@router.post("/post/post-id/reply", response_model=ReplyOut)
+def create_reply(
+    post_id: int,
+    new_reply: NewReplyIn,
+    user=Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    """
+    # Create new reply
+    Creates reply to specified post and stores it in the database.
+
+    ---
+
+    ### Authorization Header
+    Must include:
+    ```
+    {
+        "Authorization": "Bearer {token}"
+    }
+    ```
+    """
+
+    reply = Reply(
+        body=new_reply.body,
+        user_id=user.id,
+        username=user.username,
+        post_id=post_id,
+    )
+    db.add(reply)
+    db.commit()
+    return reply
+
+
+@router.get("/post/<post-id>/replies", response_model=List[ReplyOut])
+def get_replies(
+    post_id: int,
+    skip: int = 0,
+    limit: int = Query(10, ge=0, le=25),
+    sort_newest_first: bool = Query(True, alias="sort-newest-first"),
+    db: Session = Depends(get_db),
+):
+    """
+    # Returns all replies to specified post
+    Use skip and limit for pagination.\\
+    Sortable by date created (by default returns newest).
+    """
+
+    if sort_newest_first:
+        replies = (
+            db.query(Reply)
+            .order_by(desc(Reply.date_created))
+            .filter(Reply.post_id == post_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
+
+    else:
+        replies = (
+            db.query(Reply)
+            .order_by(asc(Reply.date_created))
+            .filter(Reply.post_id == post_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
+
+    return replies
+
+
+@router.get("/posts/recent", response_model=List[PostOut])
+def get_recent_posts(
+    skip: int = 0,
+    limit: int = Query(10, ge=0, le=25),
+    db: Session = Depends(get_db),
+):
+    """
+    # Returns list of recent posts from all users
+    Useful for a home/front page blog site before login
+    """
+
+    posts = (
+        db.query(Post).order_by(desc(Post.date_created)).offset(skip).limit(limit).all()
+    )
+
+    return posts
