465acd05adc3d10f3fee9a01afa4b1b59bb5a974
Zak <zakmcrae@gmail.com>
2021-04-26 16:44:34 -0400

    async rework
    util functions and tests now async

--- BlogAPI/tests/test_utils.py
+++ BlogAPI/tests/test_utils.py
@@ -1,22 +1,25 @@
 import pytest
 from fastapi.exceptions import HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy import select, func
 
 from BlogAPI.db.SQLAlchemy_models import User
+from BlogAPI.db.db_session_async import create_async_session
 from BlogAPI.tests.test_setup_and_utils import TestingSessionLocal
 from BlogAPI.util.utils import validate_new_user, authenticate_user
 
 
-def test_authenticate_user():
-    db: Session = TestingSessionLocal()
-
+@pytest.mark.asyncio
+async def test_authenticate_user():
     # successful test case user info
     username = "zaktest"
     password = "123"
-    authenticated_user = authenticate_user(db, username, password)
+    authenticated_user = await authenticate_user(username, password)
 
     # actual user info from test.db
-    user_in_db = db.query(User).filter_by(username=username).first()
+    async with create_async_session() as session:
+        query = select(User).filter(func.lower(User.username) == username.lower())
+        result = await session.execute(query)
+        user_in_db = result.scalar_one_or_none()
 
     # successful test
     assert authenticated_user == user_in_db
@@ -24,26 +27,27 @@ def test_authenticate_user():
     # wrong username case
     username = "jim"
     password = "123"
-    authenticated_user = authenticate_user(db, username, password)
+    authenticated_user = await authenticate_user(username, password)
     assert authenticated_user is False
 
     # wrong password case
     username = "zaktest"
     password = "password"
-    authenticated_user = authenticate_user(db, username, password)
+    authenticated_user = await authenticate_user(username, password)
     assert authenticated_user is False
 
 
-def test_validate_new_user():
+@pytest.mark.asyncio
+async def test_validate_new_user():
     db = TestingSessionLocal()
 
-    # fail case invalid username
+    # fail case taken username
     with pytest.raises(HTTPException):
-        validate_new_user(db, "zaktest", "jim@example.com")
+        await validate_new_user("zaktest", "jim@example.com")
 
-    # fail case invalid email
+    # fail case taken email
     with pytest.raises(HTTPException):
-        validate_new_user(db, "jim", "zaktest@example.com")
+        await validate_new_user("jim", "zaktest@example.com")
 
-    # test valid email and username
-    assert validate_new_user(db, "jim", "jim@example.com") is True
+    # test valid email and username (both not taken)
+    assert await validate_new_user("jim", "jim@example.com") is True
--- BlogAPI/util/utils.py
+++ BlogAPI/util/utils.py
@@ -1,16 +1,21 @@
 from fastapi import HTTPException
-from sqlalchemy import func
+from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 from starlette import status
 
 from BlogAPI.db.SQLAlchemy_models import User
+from BlogAPI.db.db_session_async import create_async_session
 
 
-def authenticate_user(db: Session, username: str, password: str) -> User:
+async def authenticate_user(username: str, password: str) -> User:
     """
     Make sure username is in database and password matches hashed password in database
     """
-    user = db.query(User).filter(func.lower(User.username) == username.lower()).first()
+    async with create_async_session() as session:
+        query = select(User).filter(func.lower(User.username) == username.lower())
+        result = await session.execute(query)
+        user = result.scalar_one_or_none()
+
     if not user:
         return False
 
@@ -20,25 +25,28 @@ def authenticate_user(db: Session, username: str, password: str) -> User:
     return user
 
 
-def validate_new_user(db: Session, username: str, email: str) -> bool:
+async def validate_new_user(username: str, email: str) -> bool:
     """
     Makes sure username and email are not already taken in database
     """
+    async with create_async_session() as session:
+        query = select(User.username).filter(
+            func.lower(User.username) == username.lower()
+        )
+        result = await session.execute(query)
+        db_username = result.scalar_one_or_none()
 
-    db_username = (
-        db.query(User.username)
-        .filter(func.lower(User.username) == username.lower())
-        .scalar()
-    )
     if db_username:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Username is taken, please try another",
         )
 
-    db_email = (
-        db.query(User.email).filter(func.lower(User.email) == email.lower()).scalar()
-    )
+    async with create_async_session() as session:
+        query = select(User.email).filter(func.lower(User.email) == email.lower())
+        result = await session.execute(query)
+        db_email = result.scalar_one_or_none()
+
     if db_email:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
