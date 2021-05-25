3fb300c59875256cf862cf65f1a45960c5ced2ee
Zak <zakmcrae@gmail.com>
2021-05-25 13:27:00 -0400

    moved db path to config.py
    Can quickly uncomment testing url for testing
    had issues monkeypatching to test.db - not worth time cost now

--- BlogAPI/db/db_session_async.py
+++ BlogAPI/db/db_session_async.py
@@ -1,16 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
-# todo - change conn string to production not testing
-# Production db
-# SQLALCHEMY_DATABASE_URL = (
-#     r"sqlite:///C:\Users\Zak\PycharmProjects\BlogAPI\BlogAPI\db\blog.db"
-# )
+from BlogAPI.config import config_settings
 
 
 def create_async_session() -> AsyncSession:
     # This points the api/test client to test.db instead of blog.db
-    SQLALCHEMY_DATABASE_URL = r"sqlite+aiosqlite:///C:\Users\Zak\PycharmProjects\BlogAPI\BlogAPI\tests\test.db"
+    SQLALCHEMY_DATABASE_URL = fr"sqlite+aiosqlite:///{config_settings.db_path}"
 
     async_engine = create_async_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
