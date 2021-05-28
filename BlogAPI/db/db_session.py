db5231727d04867371ba173f502e7ba5c88d6241
Zak <zakmcrae@gmail.com>
2021-05-28 14:16:12 -0400

    renamed database path for clarity

--- BlogAPI/db/db_session.py
+++ BlogAPI/db/db_session.py
@@ -4,7 +4,7 @@ from BlogAPI.config import config_settings
 
 # this non async db session is used for creating initial tables
 # see db_session_async for async engine/session code that is used by the api
-SQLALCHEMY_DATABASE_URL = fr"sqlite:///{config_settings.db_path}"
+SQLALCHEMY_DATABASE_URL = fr"sqlite:///{config_settings.database_file_path}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
